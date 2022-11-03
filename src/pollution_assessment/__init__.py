492a608eaa70c931df1db6849e60d295e3bdc5df
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2022-11-03 17:49:47 -0500

    Updates enabling comparisons to older data input files

--- src/pollution_assessment/__init__.py
+++ src/pollution_assessment/__init__.py
@@ -2,4 +2,4 @@
 __version__ = '0.1.0'
 
 # populate package namespace
-from pollution_assessment import calc, plot, dynamic_plot
+from pollution_assessment import calc, plot, dynamic_plot, plot_protected_land, summary_stats
